2efc683c22f79ed9d979a43655784391c8d667a7
mzinnvo-code <275992633+mzinnvo-code@users.noreply.github.com>
2026-04-22 13:27:18 -0400

    Bump service worker cache for class notes update

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
 
-const CACHE = "boricua-flashcards-v1";
+const CACHE = "boricua-flashcards-v2";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())
+  );
 });
 self.addEventListener("fetch", event => {
   event.respondWith(
